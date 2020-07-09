88db13fb8508c7dd2d8e1f32178eca8afb153cf2
zaj <724755391@qq,com>
2020-07-09 10:53:26 +0800

    stringFormat 学习

--- src/main/java/com/aj/tool/study/function/Main.java
+++ src/main/java/com/aj/tool/study/function/Main.java
@@ -13,6 +13,31 @@ import java.util.function.Function;
  */
 public class Main {
 
+
+    public static void main(String[] args) throws Exception {
+        stringFormatStudy();
+    }
+
+    private static void stringFormatStudy() {
+        String str;
+        str=String.format("Hi,%s", "小超");
+        System.out.println(str);
+        str=String.format("Hi,%s %s %s", "小超","是个","大帅哥");
+        System.out.println(str);
+        System.out.printf("字母c的大写是：%c %n", 'C');
+        System.out.printf("布尔结果是：%b %n", "小超".equals("帅哥"));
+        System.out.printf("100的一半是：%d %n", 100/2);
+        System.out.printf("100的16进制数是：%x %n", 100);
+        System.out.printf("100的8进制数是：%o %n", 100);
+        System.out.printf("50元的书打8.5折扣是：%f 元%n", 50*0.85);
+        System.out.printf("上面价格的16进制数是：%a %n", 50*0.85);
+        System.out.printf("上面价格的指数表示：%e %n", 50*0.85);
+        System.out.printf("上面价格的指数和浮点数结果的长度较短的是：%g %n", 50*0.85);
+        System.out.printf("上面的折扣是%d%% %n", 85);
+        System.out.printf("字母A的散列码是：%h %n", 'A');
+    }
+
+
     public static void test01() throws Exception {
         Runnable r = () -> System.out.println("-------------------->>>>>>>>>>>>>>");
         r.run();
@@ -37,33 +62,30 @@ public class Main {
         System.out.println(f1.apply(100));
     }
 
-    static int get(){
+    static int get() {
         return 1;
     }
-    static void exec(){
+
+    static void exec() {
     }
-
-    public static void main(String[] args) throws Exception {
-        Runnable r1 = Main::get;
-        Runnable r2 = Main::exec;
-        r1.run();
-        r2.run();
-
-    }
-
 }
 
 @FunctionalInterface
-interface Foo{
+interface Foo {
     int get();
 }
 
 @FunctionalInterface
-interface UserMapper{
+interface UserMapper {
     void insert(User user);
 }
 
 @FunctionalInterface
-interface OrderMapper{
+interface OrderMapper {
     int insert(Order order);
+}
+
+@FunctionalInterface
+interface ThreeConsumer<A,B,C> {
+    void accept(A a, B b, C c);
 }
