81de83186b403f236fd178120e6a48dc7c37e2e8
zaj <724755391@qq,com>
2020-08-15 14:37:15 +0800

    表白源码+1

---
+++ src/main/resources/static/ai/js/functions.js
@@ -0,0 +1,222 @@
+var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+$(function () {
+    $loveHeart = $("#loveHeart");
+    var a = $loveHeart.width() / 2;
+    var b = $loveHeart.height() / 2 - 55;
+    $garden = $("#garden");
+    gardenCanvas = $garden[0];
+    gardenCanvas.width = $("#loveHeart").width();
+    gardenCanvas.height = $("#loveHeart").height();
+    gardenCtx = gardenCanvas.getContext("2d");
+    gardenCtx.globalCompositeOperation = "lighter";
+    garden = new Garden(gardenCtx, gardenCanvas);
+    $("#content").css("width", $loveHeart.width() + $("#code").width());
+    $("#content").css("height", Math.max($loveHeart.height(), $("#code").height()));
+    $("#content").css("margin-top", Math.max(($window.height() - $("#content").height()) / 2, 10));
+    $("#content").css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10));
+    setInterval(function () {
+        garden.render()
+    }, Garden.options.growSpeed)
+});
+$(window).resize(function () {
+    var b = $(window).width();
+    var a = $(window).height();
+    if (b != clientWidth && a != clientHeight) {
+        location.replace(location)
+    }
+});
+
+function getHeartPoint(c) {
+    var b = c / Math.PI;
+    var a = 19.5 * (16 * Math.pow(Math.sin(b), 3));
+    var d = -20 * (13 * Math.cos(b) - 5 * Math.cos(2 * b) - 2 * Math.cos(3 * b) - Math.cos(4 * b));
+    return new Array(offsetX + a, offsetY + d)
+}
+
+function startHeartAnimation() {
+    var c = 50;
+    var d = 10;
+    var b = new Array();
+    var a = setInterval(function () {
+        var h = getHeartPoint(d);
+        var e = true;
+        for (var f = 0; f < b.length; f++) {
+            var g = b[f];
+            var j = Math.sqrt(Math.pow(g[0] - h[0], 2) + Math.pow(g[1] - h[1], 2));
+            if (j < Garden.options.bloomRadius.max * 1.3) {
+                e = false;
+                break
+            }
+        }
+        if (e) {
+            b.push(h);
+            garden.createRandomBloom(h[0], h[1])
+        }
+        if (d >= 30) {
+            clearInterval(a);
+            showMessages()
+        } else {
+            d += 0.2
+        }
+    }, c)
+}
+
+(function (a) {
+    a.fn.typewriter = function () {
+        this.each(function () {
+            var d = a(this), c = d.html(), b = 0;
+            d.html("");
+            var e = setInterval(function () {
+                var f = c.substr(b, 1);
+                if (f == "<") {
+                    b = c.indexOf(">", b) + 1
+                } else {
+                    b++
+                }
+                d.html(c.substring(0, b) + (b & 1 ? "_" : ""));
+                if (b >= c.length) {
+                    clearInterval(e)
+                }
+            }, 75)
+        });
+        return this
+    }
+})(jQuery);
+
+function timeElapse(c) {
+    var e = new Date();
+    var f = (Date.parse(e) - Date.parse(c)) / 1000;
+    var g = Math.floor(f / (3600 * 24));
+    f = f % (3600 * 24);
+    var b = Math.floor(f / 3600);
+    if (b < 10) {
+        b = "0" + b
+    }
+    f = f % 3600;
+    var d = Math.floor(f / 60);
+    if (d < 10) {
+        d = "0" + d
+    }
+    f = f % 60;
+    if (f < 10) {
+        f = "0" + f
+    }
+    // var a = '<span class="digit">' + g + '</span> days <span class="digit">' + b + '</span> hours <span class="digit">' + d + '</span> minutes <span class="digit">' + f + "</span> seconds";
+    var a = '<span class="digit">' + datedifference(c.pattern("yyyy-MM-dd"),e.pattern("yyyy-MM-dd")) + '</span> days <span class="digit">' + b + '</span> hours <span class="digit">' + d + '</span> minutes <span class="digit">' + f + "</span> seconds";
+    $("#elapseClock").html(a)
+}
+
+// 对Date的扩展，将 Date 转化为指定格式的String
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+// 例子：
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "H+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+Date.prototype.pattern=function(fmt) {
+    var o = {
+        "M+" : this.getMonth()+1, //月份
+        "d+" : this.getDate(), //日
+        "h+" : this.getHours()%12 == 0 ? 12 : this.getHours()%12, //小时
+        "H+" : this.getHours(), //小时
+        "m+" : this.getMinutes(), //分
+        "s+" : this.getSeconds(), //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S" : this.getMilliseconds() //毫秒
+    };
+    var week = {
+        "0" : "/u65e5",
+        "1" : "/u4e00",
+        "2" : "/u4e8c",
+        "3" : "/u4e09",
+        "4" : "/u56db",
+        "5" : "/u4e94",
+        "6" : "/u516d"
+    };
+    if(/(y+)/.test(fmt)){
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    }
+    if(/(E+)/.test(fmt)){
+        fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? "/u661f/u671f" : "/u5468") : "")+week[this.getDay()+""]);
+    }
+    for(var k in o){
+        if(new RegExp("("+ k +")").test(fmt)){
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+        }
+    }
+    return fmt;
+}
+
+//两个时间相差天数 兼容firefox chrome
+function datedifference(sDate1, sDate2) {    //sDate1和sDate2是2006-12-18格式
+    var dateSpan,
+        tempDate,
+        iDays;
+    sDate1 = Date.parse(sDate1);
+    sDate2 = Date.parse(sDate2);
+    dateSpan = sDate2 - sDate1;
+    dateSpan = Math.abs(dateSpan);
+    iDays = Math.floor(dateSpan / (24 * 3600 * 1000));
+    return iDays
+};
+
+function showMessages() {
+    $("#messages").fadeIn(5000, function () {
+        showLoveU()
+    })
+}
+
+function adjustWordsPosition() {
+    $("#words").css("position", "absolute");
+    $("#words").css("top", $("#garden").position().top + 195);
+    $("#words").css("left", $("#garden").position().left + 70)
+}
+
+function adjustCodePosition() {
+    $("#code").css("margin-top", ($("#garden").height() - $("#code").height()) / 2)
+}
+
+function showLoveU() {
+    $("#loveu").fadeIn(3000)
+};
+
+function playSound() {
+    var borswer = window.navigator.userAgent.toLowerCase();
+    if (borswer.indexOf("ie") >= 0) {
+        //IE内核浏览器
+        var strEmbed = '<embed name="embedPlay" src="./renxi.mp3" autostart="true" hidden="true" loop="false"></embed>';
+        if ($("body").find("embed").length <= 0)
+            $("body").append(strEmbed);
+        var embed = document.embedPlay;
+
+        //浏览器不支持 audion，则使用 embed 播放
+        embed.volume = 100;
+        embed.play();
+    } else {
+        //非IE内核浏览器
+        var strAudio = "<audio id='audioPlay' src='./renxi.mp3' hidden='true'>";
+        if ($("body").find("audio").length <= 0)
+            $("body").append(strAudio);
+        var audio = document.getElementById("audioPlay");
+
+        //浏览器支持 audion
+        audio.play();
+    }
+}
